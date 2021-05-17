14e4fa4a2199648458c18e5bdf59287d116ea458
Eriah norwood <58611170+RDV3736@users.noreply.github.com>
2021-05-17 00:51:00 +0000

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1/hackMain.h
+++ ConsoleApplication1/ConsoleApplication1/hackMain.h
@@ -2,94 +2,75 @@
 #include <iostream>
 #include "windows.h"
 #include "hackMain.h"
+#include <thread>
 #include "mFuncs.h"
 #include <vector>
 
-namespace position
-{
-
-
-    struct mat3x4
-    {
-        float* operator[ ](int index) {
-            return c[index];
-        }
-        float c[3][4];
-    };
-
-
-    mat3x4 GetBoneMatrix(DWORD entity, int boneCount)
-    {
-        DWORD BoneMatrixPtr = memory::RPM<DWORD>(entity + hazedumper::netvars::m_dwBoneMatrix);
-        return memory::RPM<mat3x4>(BoneMatrixPtr + (0x30 * boneCount));
-    }
-
-    video::mat4x4 getViewMatrix(DWORD matrixAddress)
-    {
-        return memory::RPM<video::mat4x4>(matrixAddress);
-    }
-
-    globals::vec3 GetBonePosition(DWORD entity, int BoneIndex)
-    {
-        DWORD BoneMatrixPtr = memory::RPM<DWORD>(entity + hazedumper::netvars::m_dwBoneMatrix);
-        mat3x4 BoneMatrix = memory::RPM<mat3x4>(BoneMatrixPtr + (0x30 * BoneIndex));
-        globals::vec3 i; // buffer
-        i.x = BoneMatrix.c[0][3];
-        i.y = BoneMatrix.c[1][3];
-        i.z = BoneMatrix.c[2][3];
-        
-        return i;
-    }
-}
-
 namespace Main
 {
-    void tBot()
+    bool db1 = false;
+    bool db2 = false;
+    bool db3 = false;
+    bool db4 = false;
+    bool db5 = false;
+
+    void CalcAngle(float* src, float* dst, float* angles)
     {
+        double delta[3] = { (src[0] - dst[0]), (src[1] - dst[1]), (src[2] - dst[2]) };
+        double hyp = sqrt(delta[0] * delta[0] + delta[1] * delta[1]);
+        angles[0] = (float)(asinf(delta[2] / hyp) * 57.295779513082f);
+        angles[1] = (float)(atanf(delta[1] / delta[0]) * 57.295779513082f);
+        angles[2] = 0.0f;
+        if (delta[0] >= 0.0) { angles[1] += 180.0f; }
     }
 
-	void hackMain()
-	{
+    void tBot()
+    {
+        std::cout << "parsed" << std::endl;
+        if (globalvars::triggerbot)
+        {
+            while (globalvars::triggerbot)
+            {
+                DWORD localPlr = memory::RPM<DWORD>(globals::moduleBase + hazedumper::signatures::dwLocalPlayer);
+                int localTeam = memory::RPM<int>(localPlr + hazedumper::netvars::m_iTeamNum);
+                int chID = memory::RPM<int>(localPlr + hazedumper::netvars::m_iCrosshairId);
+                if (chID != 0 && chID < 64)
+                {
+                    //int chID = RPM
+                    DWORD eList = memory::RPM<DWORD>(globals::moduleBase + hazedumper::signatures::dwEntityList + ((chID - 1) * 0x10));
+                    int eHP = memory::RPM<int>(eList + hazedumper::netvars::m_iHealth);
+                    int eTeam = memory::RPM<int>(eList + hazedumper::netvars::m_iTeamNum);
+                    if (localTeam != eTeam && eHP > 0)
+                    {
+                        std::cout << eTeam << std::endl;
+                        std::cout << localTeam << std::endl;
+                        Sleep(250);
+                        memory::WPM<int>(globals::moduleBase + hazedumper::signatures::dwForceAttack, 5);
+                        Sleep(10);
+                        memory::WPM<int>(globals::moduleBase + hazedumper::signatures::dwForceAttack, 4);
+                    }
+
+                }
+            }
+        }
+    }
+    void gdiEsp()
+    {
         HBRUSH brush = CreateSolidBrush(0x0000FF00);//0x000000FF
         HBRUSH tbrush = CreateSolidBrush(0x000000FF);//0x0000FF00
-        while (!GetAsyncKeyState(VK_END))
+        while (globalvars::gdiEsp)
         {
-            if (GetAsyncKeyState(VK_F9))
-            {
-                if (globalvars::glowEsp)
-                    globalvars::glowEsp = false;
-                else
-                    globalvars::glowEsp = true;
-            }
-            if (GetAsyncKeyState(VK_F10))
-            {
-                if (globalvars::gdiEsp)
-                    globalvars::gdiEsp = false;
-                else
-                    globalvars::gdiEsp = true;
-            }
-            uintptr_t glowManager = memory::RPM<uintptr_t>(globals::moduleBase + hazedumper::signatures::dwGlowObjectManager);
             int teamNum = memory::RPM<int>(memory::getLocalPlayer() + hazedumper::netvars::m_iTeamNum);
-            video::mat4x4 viewMatrix = memory::RPM<video::mat4x4>((globals::moduleBase) + hazedumper::signatures::dwViewMatrix);
+            video::mat4x4 viewMatrix = memory::RPM<video::mat4x4>((globals::moduleBase)+hazedumper::signatures::dwViewMatrix);
             uintptr_t localPlayer = memory::getLocalPlayer();
-
-            if (globalvars::aimAssit)
-            {
-                
-            }
-           
-            
-
-
             if (globalvars::gdiEsp)
             {
                 for (size_t i = 0; i < 64; i++)
                 {
-
                     uintptr_t enemyI = memory::RPM<uintptr_t>(globals::moduleBase + hazedumper::signatures::dwEntityList + i * 0x10);
 
                     // hp checks here n all
-                    
+
                    // if (enemyI == NULL)
                    //     continue;
 
@@ -99,32 +80,10 @@ namespace Main
 
                     if (enemyHP <= 0)
                         continue;
-
-
-
                     int eTeamNum = memory::RPM<int>(localPlayer + hazedumper::netvars::m_iTeamNum);
-
-                    if (globalvars::triggerbot == true)
-                    {
-                        int crosshair = memory::RPM<int>(localPlayer + hazedumper::netvars::m_iCrosshairId);
-                        if (crosshair != 0 && crosshair < 64)
-                        {
-                            DWORD enT = memory::RPM<DWORD>(enemyI + crosshair);
-                            int enTTeam = memory::RPM<int>(enemyI + hazedumper::netvars::m_iTeamNum);
-                            if (enTTeam != eTeamNum && enemyHP > 0)
-                            {
-                                // if they're not on our team
-                                memory::WPM<int>(globals::moduleBase + hazedumper::signatures::dwForceAttack, 5);
-                                Sleep(1);
-                                memory::WPM<int>(globals::moduleBase + hazedumper::signatures::dwForceAttack, 4);
-                            }
-                        }
-                    }
 
                     if (teamNum != eTeamNum && enemyI != localPlayer)
                     {
-                     
-
                         globals::vec3 pos = memory::RPM<globals::vec3>(enemyI + hazedumper::netvars::m_vecOrigin);
                         globals::vec3 head;
                         //globals::vec3 ret = video::WorldToScreen(ePos,viewMatrix);
@@ -138,7 +97,9 @@ namespace Main
                         float width = height / 2.4f;
                         if (screenpos.z >= 0.01f)
                         {
-                            video::DrawBorderBox(screenpos.x - (width / 2), screenpos.y, width, height, 1, brush);
+                            video::DrawBorderBox(screenpos.x - (width / 2), screenpos.y, width, height, 2, brush);
+                            int chp = (width / 100) * enemyHP;
+                            video::DrawFilledRect((screenpos.x - (width / 2)) - 10, screenpos.y, (width / 9), (chp * 2.5), brush);
                         }
                     }
                     else
@@ -160,12 +121,22 @@ namespace Main
                             if (screenpos.z >= 0.01f)
                             {
                                 video::DrawBorderBox(screenpos.x - (width / 2), screenpos.y, width, height, 1, tbrush);
-                            }
+
+                                int chp = (width / 100) * enemyHP;
+                                video::DrawFilledRect(screenpos.x - (width / 2), screenpos.y, (width / 9), (chp * 2.5), brush);
+                            }                                                                            
                         }
                     }
                 }
             }
-
+        }
+    }
+    void glowEsp()
+    {
+        while (globalvars::glowEsp)
+        {
+            uintptr_t glowManager = memory::RPM<uintptr_t>(globals::moduleBase + hazedumper::signatures::dwGlowObjectManager);
+            int teamNum = memory::RPM<int>(memory::getLocalPlayer() + hazedumper::netvars::m_iTeamNum);
             if (globalvars::glowEsp)
             {
                 for (size_t i = 0; i <= 64; i++)
@@ -211,6 +182,125 @@ namespace Main
                 }
 
             }
+        }
+    }
+    void boneESP()
+    {
+        HBRUSH brush = CreateSolidBrush(0x0000FF00);//0x000000FF
+        HBRUSH tbrush = CreateSolidBrush(0x000000FF);//0x0000FF00
+        while (globalvars::boneEsp)
+        {
+            int teamNum = memory::RPM<int>(memory::getLocalPlayer() + hazedumper::netvars::m_iTeamNum);
+            video::mat4x4 viewMatrix = memory::RPM<video::mat4x4>((globals::moduleBase)+hazedumper::signatures::dwViewMatrix);
+            uintptr_t localPlayer = memory::getLocalPlayer();
+            for (size_t i = 0; i < 64; i++)
+            {
+                // modeltype
+                char* operatorN[128];
+                uintptr_t enemyI = memory::RPM<uintptr_t>(globals::moduleBase + hazedumper::signatures::dwEntityList + i * 0x10);
+                DWORD boneBase = memory::RPM<DWORD>(enemyI + hazedumper::netvars::m_dwBoneMatrix);
+               // char* operatorName = memory::RPM<char*>(*(int*)(enemyI + 0x6C) + 0x04);
+               // strcpy(*operatorN, operatorName);
+               // std::string name(*operatorN);// now make into string
+
+                const char* Separatist = "tm_separatist";
+                // main
+                video::DrawLine(500,200,510,600,10);
+
+              
+
+            }
+        }
+    }
+    void aimBot()
+    {
+        while (globalvars::aimAssit)
+        {
+            int teamNum = memory::RPM<int>(memory::getLocalPlayer() + hazedumper::netvars::m_iTeamNum);
+            video::mat4x4 viewMatrix = memory::RPM<video::mat4x4>((globals::moduleBase)+hazedumper::signatures::dwViewMatrix);
+            uintptr_t localPlayer = memory::getLocalPlayer();
+            globals::vec3 enemyCoordinate;
+            globals::vec3 localCoordinate;
+            globals::vec3 totalCoordinate;
+
+            for (size_t i = 0; i < 64; i++)
+            {
+                uintptr_t enemyI = memory::RPM<uintptr_t>(globals::moduleBase + hazedumper::signatures::dwEntityList + i * 0x10);
+                localCoordinate = video::GetBonePosition(localPlayer,6);
+                enemyCoordinate = video::GetBonePosition(enemyI, 6);
+
+                totalCoordinate.x = enemyCoordinate.x - localCoordinate.x;
+                totalCoordinate.y = enemyCoordinate.y - localCoordinate.y;
+                totalCoordinate.z = enemyCoordinate.z - localCoordinate.z;
+
+                float add = sqrt(pow(totalCoordinate.x, 2) + pow(totalCoordinate.y, 2));
+
+                float posA = totalCoordinate.x;
+                float posB = totalCoordinate.y;
+                float posC = add;
+
+                float yaw = atan(posB / posA);
+                float pitch = -atan(0 / posC);
+                std::cout << "Pitch: " << pitch << std::endl;
+                std::cout << "Yaw: " << yaw << std::endl;
+                system("cls");
+
+            }
+
+
+        }
+    }
+    
+    void hackMain()
+	{
+        while (!GetAsyncKeyState(VK_END))
+        {
+            if (GetAsyncKeyState(VK_F9))
+            {
+                if (globalvars::glowEsp)
+                    globalvars::glowEsp = false;
+                else
+                    globalvars::glowEsp = true;
+            }
+            if (GetAsyncKeyState(VK_F10))
+            {
+                if (globalvars::gdiEsp)
+                    globalvars::gdiEsp = false;
+                else
+                    globalvars::gdiEsp = true;
+            }
+            if (globalvars::aimAssit)
+            {
+                
+            }
+          
+            if (db1 == false)
+            {
+                CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)tBot, NULL,NULL,NULL);
+                db1 = true;
+            }
+            if (db2 == false)
+            {
+                CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)gdiEsp, NULL,NULL,NULL);
+                db2 = true;
+            }
+            if (db3 == false)
+            {
+                CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)glowEsp, NULL,NULL,NULL);
+                db3 = true;
+            }
+            if (db4 == false)
+            {
+                CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)boneESP, NULL, NULL, NULL);
+                db4 = true;
+            }
+            if (db5 == false)
+            {
+                CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)aimBot , NULL, NULL, NULL);
+                db5 = true;
+            }
+
+            
         }
 	}
 }
--- ConsoleApplication1/ConsoleApplication1/mFuncs.h
+++ ConsoleApplication1/ConsoleApplication1/mFuncs.h
@@ -6,6 +6,16 @@
 #include <gdiplus.h>
 #pragma comment (lib,"Gdiplus.lib")
 
+namespace globalvars
+{
+	bool triggerbot = false;
+	bool boneEsp = true;
+	bool glowEsp = false;
+	bool gdiEsp = false;
+	bool aimAssit = true;
+}
+
+
 namespace globals
 {
 	struct lGlowStructEnemy
@@ -41,9 +51,21 @@ namespace globals
 
 	struct vec3
 	{
+		vec3 operator- (const vec3& b) {
+			this->x - b.x;
+			this->y - b.y;
+			this->z - b.z;
+			return *this;
+		}
 		float x = 0;
 		float y = 0;
 		float z = 0;
+	};
+
+	struct vec2
+	{
+		float x = 0;
+		float y = 0;
 	};
 	//tool
 	DWORD getModule(LPCSTR ModuleName, UINT32 dwPID)
@@ -86,79 +108,6 @@ namespace globals
 
 }
 
-namespace globalvars
-{
-	bool triggerbot = false;
-	bool boneEsp = true;
-	bool glowEsp = true;
-	bool gdiEsp = false;
-	bool aimAssit = true;
-}
-
-namespace video
-{
-	float slope(float x1, float y1, float x2, float y2)
-	{
-		return (y2 - y1) / (x2 - x1);
-	}
-
-
-	void DrawFilledRect(int x, int y, int w, int h, HBRUSH brush)
-	{
-		RECT rect = { x, y, x + w, y + h };
-		FillRect(globals::hdc , &rect, brush);
-	}
-
-	void DrawLine(float x1, float y1, float x2, float y2, float thickness)
-	{
-		float s = slope(x1,y1,x2,y2);
-		float distBetween = x1 - x2;
-		HBRUSH brush = CreateSolidBrush(0x00FFFFFF);
-		for (size_t i = x1; i < x2; i++)
-		{
-			DrawFilledRect(i,(y1 - s), thickness,thickness,brush);
-		}
-	}
-
-	void DrawBorderBox(int x, int y, int w, int h, int thickness, HBRUSH brush)
-	{
-		DrawFilledRect(x, y, w, thickness, brush); //Top horiz line
-		DrawFilledRect(x, y, thickness, h, brush); //Left vertical line
-		DrawFilledRect((x + w), y, thickness, h, brush); //right vertical line
-		DrawFilledRect(x, y + h, w + thickness, thickness, brush); //bottom horiz line
-	}
-
-	struct mat4x4
-	{
-		float* operator[ ](int index) {
-			return matrix[index];
-		}
-		float matrix[4][4];
-	};
-
-	globals::vec3 WorldToScreen(globals::vec3 pos, mat4x4 matrix) 
-	{
-		float _x = matrix[0][0] * pos.x + matrix[0][1] * pos.y + matrix[0][2] * pos.z + matrix[0][3];
-		float _y = matrix[1][0] * pos.x + matrix[1][1] * pos.y + matrix[1][2] * pos.z + matrix[1][3];
-
-		float w = matrix[3][0] * pos.x + matrix[3][1] * pos.y + matrix[3][2] * pos.z + matrix[3][3];
-		
-		float inv_w = 1.f / w;
-		_x *= inv_w;
-		_y *= inv_w;
-
-		float x = globals::screenX * .5f;
-		float y = globals::screenY * .5f;
-
-		x += 0.5f * _x * globals::screenX + 0.5f;
-		y -= 0.5f * _y * globals::screenY + 0.5f;
-
-		return { x,y,w };
-	}
-
-
-}
-
 namespace memory
 {
 	template<typename T>
@@ -178,5 +127,169 @@ namespace memory
 	{
 		return RPM<uintptr_t>(hazedumper::signatures::dwLocalPlayer + globals::moduleBase);
 	}
+
+}
+namespace video
+{
+	float slope(float x1, float y1, float x2, float y2)
+	{
+		return (y2 - y1) / (x2 - x1);
+	}
+
+
+	void DrawFilledRect(int x, int y, int w, int h, HBRUSH brush)
+	{
+		RECT rect = { x, y, x + w, y + h };
+		FillRect(globals::hdc , &rect, brush);
+	}
+
+	void DrawPixel(float x, float y, int thickness)
+	{
+		HBRUSH brush = CreateSolidBrush(0x00FFFFFF);
+		DrawFilledRect(x, y,thickness, thickness, brush);
+	}
+
+	void DrawText(std::string text,int x,int y, int w, int h, HBRUSH brush = CreateSolidBrush(0x00FFFFFF))
+	{
+		RECT rect = { x, y, w, h };
+		DrawText(globals::hdc,text.c_str(),sizeof(text),&rect, DT_CALCRECT || DT_NOCLIP);
+	}
+
+	int Sign(float x) {
+		if (x < 0)
+		{
+			return -1;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+
+	void DrawLine(float originx, float originy, float endx, float endy, float thickness, HBRUSH brush = CreateSolidBrush(0x00FFFFFF))
+	{
+		float distBetween = originx - endy;
+
+		float deltax = endx - originx;
+		float deltay = endy - originy;
+
+		abs(deltax / deltay);
+		
+		int Y = originy;
+		int eps = 0;
+
+		
+
+		for (int x = originx; x <= endx; x++)
+		{
+			DrawPixel(x,Y,thickness);
+			eps += deltay;
+			if ((eps << 1) >= deltax)
+			{
+				Y++;  eps -= deltax;
+			}
+		}
+
+		//float x1 = originx;
+		//float y1 = originy;
+		//float x2 = endx;
+		//float y2 = endy;
+		//float dy = y2 - y1;
+		//float dx = x2 - x1;
+
+		//float y;
+		//float x;
+
+		//if (fabs(dy) > fabs(dx))
+		//{
+		//	for (y = y1; y != y2; y += Sign(dy))
+		//	{
+		//		x = x1 + (y - y1) * dx / dy;
+		//		DrawPixel(x, y, thickness);
+
+		//	}
+		//}
+
+	}
+
+	void DrawBorderBox(int x, int y, int w, int h, int thickness, HBRUSH brush)
+	{
+		DrawFilledRect(x, y, w, thickness, brush); //Top horiz line
+		DrawFilledRect(x, y, thickness, h, brush); //Left vertical line
+		DrawFilledRect((x + w), y, thickness, h, brush); //right vertical line
+		DrawFilledRect(x, y + h, w + thickness, thickness, brush); //bottom horiz line
+	}
+
+	struct mat4x4
+	{
+		float* operator[ ](int index) {
+			return matrix[index];
+		}
+		float matrix[4][4];
+	};
+	struct mat3x4
+	{
+		float* operator[ ](int index) {
+			return c[index];
+		}
+		float c[3][4];
+	};
+	globals::vec3 WorldToScreen(globals::vec3 pos, mat4x4 matrix) 
+	{
+		float _x = matrix[0][0] * pos.x + matrix[0][1] * pos.y + matrix[0][2] * pos.z + matrix[0][3];
+		float _y = matrix[1][0] * pos.x + matrix[1][1] * pos.y + matrix[1][2] * pos.z + matrix[1][3];
+		float w = matrix[3][0] * pos.x + matrix[3][1] * pos.y + matrix[3][2] * pos.z + matrix[3][3];
+		float inv_w = 1.f / w;
+		_x *= inv_w;
+		_y *= inv_w;
+		float x = globals::screenX * .5f;
+		float y = globals::screenY * .5f;
+		x += 0.5f * _x * globals::screenX + 0.5f;
+		y -= 0.5f * _y * globals::screenY + 0.5f;
+
+		return { x,y,w };
+	}
+
+	mat3x4 GetBoneMatrix(DWORD entity, int boneCount)
+	{
+		DWORD BoneMatrixPtr = memory::RPM<DWORD>(entity + hazedumper::netvars::m_dwBoneMatrix);
+		return memory::RPM<mat3x4>(BoneMatrixPtr + (0x30 * boneCount));
+	}
+
+	video::mat4x4 getViewMatrix(DWORD matrixAddress)
+	{
+		return memory::RPM<video::mat4x4>(matrixAddress);
+	}
+
+	globals::vec3 GetBonePosition(DWORD entity, int BoneIndex)
+	{
+		DWORD BoneMatrixPtr = memory::RPM<DWORD>(entity + hazedumper::netvars::m_dwBoneMatrix);
+		mat3x4 BoneMatrix = memory::RPM<mat3x4>(BoneMatrixPtr + (0x30 * BoneIndex));
+		globals::vec3 i; // buffer
+		i.x = BoneMatrix.c[0][3];
+		i.y = BoneMatrix.c[1][3];
+		i.z = BoneMatrix.c[2][3];
+
+		return i;
+	}
+
+	void DrawBone(DWORD boneBase,mat4x4 viewMatrix, int bone1, int bone2)
+	{
+		globals::vec3 bone1_pos;
+		globals::vec3 w2sb1;
+		bone1_pos = GetBonePosition(boneBase, bone1);
+
+		globals::vec3 bone2_pos;
+		globals::vec3 w2sb2;
+		bone1_pos = GetBonePosition(boneBase, bone2);
+
+		w2sb1 = WorldToScreen(bone1_pos, viewMatrix);
+		w2sb2 = WorldToScreen(bone2_pos, viewMatrix);
+
+		DrawLine(w2sb1.x, w2sb1.y,w2sb2.x,w2sb2.y, 2);
+
+
+	}
+
 
 }
